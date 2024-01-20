73cdd491a685b806fbe901e2b096d91b9660925d
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-20 14:58:32 +0800

    Added button

---
+++ app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const HomePage = () => {
+	return (
+		<div className='p-4'>
+			<Button>Click Me</Button>
+		</div>
+	);
+};
+
+export default HomePage;
